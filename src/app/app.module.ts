b3ca1948d71e2100e6a4b974a72ddfd399e5043c
heshanu <heshanu97@gmail.com>
2022-04-29 21:17:57 +0530

    added form arryas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { ObservComponent } from './observ/observ.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { ReactiveFormV2Component } from './reactive-form-v2/reactive-form-v2.component';
+import { InvoiceComponent } from './invoice/invoice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ObservComponent,
     ReactiveFormComponent,
-    ReactiveFormV2Component
+    ReactiveFormV2Component,
+    InvoiceComponent
   ],
   imports: [
     BrowserModule,
